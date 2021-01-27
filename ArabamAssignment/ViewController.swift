a979318ebdda55c0b18b390f700c08b9e4002e39
Furkan Arslan <fur.arslan@gmail.com>
2021-01-27 16:17:06 +0300

    base project & readme added for the assignment

---
+++ ArabamAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArabamAssignment
+//
+//  Created by Furkan Arslan on 25.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
