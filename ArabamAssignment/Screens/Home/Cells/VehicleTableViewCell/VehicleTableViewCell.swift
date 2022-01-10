e08a92dc660d60921031870a306f4839fec126ef
Muhammed Karakul <muhammedkarakul@Muhammeds-MacBook-Pro.local>
2022-01-10 23:31:12 +0300

    VehicleTableViewCell added.

---
+++ ArabamAssignment/Screens/Home/Cells/VehicleTableViewCell/VehicleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  VehicleTableViewCell.swift
+//  ArabamAssignment
+//
+//  Created by Muhammed Karakul on 10.01.2022.
+//
+
+import UIKit
+
+final class VehicleTableViewCell: UITableViewCell {
+    @IBOutlet weak var vehicleImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+}
--- ArabamAssignment/Screens/Home/Controller/HomeViewController.swift
+++ ArabamAssignment/Screens/Home/Controller/HomeViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = "Home"
+        tableView.register(UINib(nibName: "VehicleTableViewCell", bundle: nil), forCellReuseIdentifier: "VehicleTableViewCell")
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VehicleTableViewCell", for: indexPath) as! VehicleTableViewCell
+        return cell
     }
 }
